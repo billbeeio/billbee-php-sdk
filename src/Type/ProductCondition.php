69b41288fbe2c155767c2b349fe822d4d3aa0c05
Julian Finkler <julian@billbee.io>
2019-12-09 09:23:14 +0100

    Added additional fields to the Product model (#32)
    Added additional fields to the Product model
    
    - `Product::$condition`
    - `Product::$widthCm`
    - `Product::$lengthCm`
    - `Product::$heightCm`
    - `Product::$billOfMaterial`
    
    Solves Issue #30

---
+++ src/Type/ProductCondition.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the Billbee API package.
+ *
+ * Copyright 2017 - 2019 by Billbee GmbH
+ *
+ * For the full copyright and license information, please read the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Created by Julian Finkler <julian@billbee.io>
+ */
+
+namespace BillbeeDe\BillbeeAPI\Type;
+
+class ProductCondition
+{
+    const BRAND_NEW = 1;
+    const USED_LIKE_NEW = 2;
+    const USED_VERY_GOOD = 3;
+    const USED_GOOD = 4;
+    const USED_IN_ORDER = 5;
+    const USED_BAD = 6;
+    const BROKEN = 7;
+}
