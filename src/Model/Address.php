2f49b9b42a356717a6dde1528e3b69d3fdf43264
Julian Finkler <julian@billbee.io>
2020-10-02 18:05:26 +0200

    Copyright updated (#34)

--- src/Model/Address.php
+++ src/Model/Address.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the Billbee API package.
  *
- * Copyright 2017 - 2019 by Billbee GmbH
+ * Copyright 2017 - 2020 by Billbee GmbH
  *
  * For the full copyright and license information, please read the LICENSE
  * file that was distributed with this source code.
